bd7b23f035a230242f4a0f1ddcf4151fa4e769c4
nickbeaird <nbeaird@cloudboltsoftware.com>
2019-11-06 20:02:51 -0800

    Finish the first assignment
    Add the a basic greedy algorithm that looks for the heaviest item in a collection.

---
+++ pset1/ps1.py
@@ -0,0 +1,145 @@
+###########################
+# 6.00.2x Problem Set 1: Space Cows 
+
+from ps1_partition import get_partitions
+import time
+
+#================================
+# Part A: Transporting Space Cows
+#================================
+
+def load_cows(filename):
+    """
+    Read the contents of the given file.  Assumes the file contents contain
+    data in the form of comma-separated cow name, weight pairs, and return a
+    dictionary containing cow names as keys and corresponding weights as values.
+
+    Parameters:
+    filename - the name of the data file as a string
+
+    Returns:
+    a dictionary of cow name (string), weight (int) pairs
+    """
+
+    cow_dict = dict()
+
+    f = open(filename, 'r')
+    
+    for line in f:
+        line_data = line.split(',')
+        cow_dict[line_data[0]] = int(line_data[1])
+    return cow_dict
+
+
+# Problem 1
+def greedy_cow_transport(cows,limit=10):
+    """
+    Uses a greedy heuristic to determine an allocation of cows that attempts to
+    minimize the number of spaceship trips needed to transport all the cows. The
+    returned allocation of cows may or may not be optimal.
+    The greedy heuristic should follow the following method:
+
+    1. As long as the current trip can fit another cow, add the largest cow that will fit
+        to the trip
+    2. Once the trip is full, begin a new trip to transport the remaining cows
+
+    Does not mutate the given dictionary of cows.
+
+    Parameters:
+    cows - a dictionary of name (string), weight (int) pairs
+    limit - weight limit of the spaceship (an int)
+    
+    Returns:
+    A list of lists, with each inner list containing the names of cows
+    transported on a particular trip and the overall list containing all the
+    trips
+    """
+    from collections import defaultdict
+    import operator
+    
+    # Convert to default dict as to not mutate the original object
+    cow_trips = []
+    tuple_list = [(k,v) for k,v in cows.items()]
+    reverse_sorted_tuple_list = sorted(tuple_list, key=lambda x: x[1], reverse=True)
+
+    while reverse_sorted_tuple_list:
+        trip = []
+        trip_weight = 0
+        new_copy = reverse_sorted_tuple_list.copy()
+
+        for cow in new_copy:
+            cow_weight = cow[1]
+            cow_name = cow[0]
+            if trip_weight + cow_weight <= limit:
+                trip_weight += cow_weight
+                trip.append(cow_name)
+                reverse_sorted_tuple_list.remove(cow) 
+        cow_trips.append(trip)        
+    return cow_trips
+
+# Problem 2
+def brute_force_cow_transport(cows,limit=10):
+    """
+    Finds the allocation of cows that minimizes the number of spaceship trips
+    via brute force.  The brute force algorithm should follow the following method:
+
+    1. Enumerate all possible ways that the cows can be divided into separate trips
+    2. Select the allocation that minimizes the number of trips without making any trip
+        that does not obey the weight limitation
+            
+    Does not mutate the given dictionary of cows.
+
+    Parameters:
+    cows - a dictionary of name (string), weight (int) pairs
+    limit - weight limit of the spaceship (an int)
+    
+    Returns:
+    A list of lists, with each inner list containing the names of cows
+    transported on a particular trip and the overall list containing all the
+    trips
+    """
+    # TODO: Your code here
+    
+
+        
+# Problem 3
+def compare_cow_transport_algorithms():
+    """
+    Using the data from ps1_cow_data.txt and the specified weight limit, run your
+    greedy_cow_transport and brute_force_cow_transport functions here. Use the
+    default weight limits of 10 for both greedy_cow_transport and
+    brute_force_cow_transport.
+    
+    Print out the number of trips returned by each method, and how long each
+    method takes to run in seconds.
+
+    Returns:
+    Does not return anything.
+    """
+    # TODO: Your code here
+    pass
+
+
+"""
+Here is some test data for you to see the results of your algorithms with. 
+Do not submit this along with any of your answers. Uncomment the last two
+lines to print the result of your problem.
+"""
+
+cows = load_cows("ps1_cow_data.txt")
+limit=10
+print(cows)
+
+print(greedy_cow_transport(cows, limit))
+
+print('\n')
+
+# cows_4 = {'Luna': 41, 'Starlight': 54, 'Willow': 59, 'Abby': 28, 'Betsy': 39, 'Rose': 42, 'Coco': 59, 'Buttercup': 11}
+# limit = 120
+# print(cows_4)
+# print(greedy_cow_transport(cows_4, limit))
+
+
+# print(brute_force_cow_transport(cows, limit))
+
+
